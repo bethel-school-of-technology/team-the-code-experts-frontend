409fe4f4aed9028ec4121e0b5997d467c4d2e03f
Josiah Boldt <orielspace@gmail.com>
2022-07-05 18:36:44 -0700

    completed skeleton structure
    completed skeleton structure

---
+++ src/app/change-username/change-username.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-username',
+  templateUrl: './change-username.component.html',
+  styleUrls: ['./change-username.component.css']
+})
+export class ChangeUsernameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
