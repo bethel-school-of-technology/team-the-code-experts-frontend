7f3c9ee45865bcd5dfbdcff244ef69469b9b989c
Josiah Boldt <orielspace@gmail.com>
2022-07-13 19:19:01 -0700

    Added "edit username" logic

--- src/app/components/settings/change-username/change-username.component.ts
+++ src/app/components/settings/change-username/change-username.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UpdateUsernameService } from 'src/app/services/update-username.service';
 
 @Component({
   selector: 'app-change-username',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-username.component.css']
 })
 export class ChangeUsernameComponent implements OnInit {
+  public updateInfo !: FormGroup;
+  public usernamesMatch = true;
+  constructor(private formBuilder: FormBuilder, private updateUsernameService: UpdateUsernameService ) {
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+    this.updateInfo = this.formBuilder.group({
+      oldUsername: ["", Validators.required],
+      newUsername: ["", Validators.required],
+      confirmUsername: ["", Validators.required]
+    })
   }
+
+
+  update():any  {
+    if (this.updateInfo.value.newUsername !== this.updateInfo.value.confirmUsername) return this.usernamesMatch = false;
+    this.updateUsernameService.updateUsername({
+      username: this.updateInfo.value.newUsername
+    }).subscribe(async (res) => {
+      console.log(res)
+    })
+  }
+
 
 }
