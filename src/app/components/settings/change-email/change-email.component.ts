cba8f724291a53335fffb8f4c37e70caf4dcee71
Josiah Boldt <orielspace@gmail.com>
2022-07-26 15:23:37 -0700

    added sytling and routing

---
+++ src/app/components/settings/change-email/change-email.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-email',
+  templateUrl: './change-email.component.html',
+  styleUrls: ['./change-email.component.css']
+})
+export class ChangeEmailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
